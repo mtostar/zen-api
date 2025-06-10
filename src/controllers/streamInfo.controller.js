1d62b8aa85e5d3bafabb1057b81c2f3520e11ba3
ANIMEGERS! <134526427+animegers@users.noreply.github.com>
2025-06-10 14:53:05 +0600

    Update streamInfo.controller.js

--- src/controllers/streamInfo.controller.js
+++ src/controllers/streamInfo.controller.js
@@ -1,17 +1,1 @@
-import { extractStreamingInfo } from "../extractors/streamInfo.extractor.js";
 
-export const getStreamInfo = async (req) => {
-  try {
-    const input = req.query.id;
-    const server = req.query.server;
-    const type = req.query.type;
-    const match = input.match(/ep=(\d+)/);
-    if (!match) throw new Error("Invalid URL format");
-    const finalId = match[1];
-    const streamingInfo = await extractStreamingInfo(finalId, server, type);
-    return streamingInfo;
-  } catch (e) {
-    console.error(e);
-    return e;
-  }
-};
